56f3779d904c81b965f0c64949219d56c527e6c1
Pierre Gordon <pierregordon@protonmail.com>
2021-09-16 12:49:22 -0400

    Add more curfews

--- curfews.js
+++ curfews.js
@@ -4,6 +4,51 @@ const curfews = [
         to: 'Sep 17, 2021',
         duration: '7:00 PM - 5:00 AM',
     },
+    {
+        from: 'Sep 18, 2021',
+        to: 'Sep 18, 2021',
+        duration: '6:00 PM - 5:00 AM',
+    },
+    {
+        from: 'Sep 20, 2021',
+        to: 'Sep 24, 2021',
+        duration: '8:00 PM - 5:00 AM',
+    },
+    {
+        from: 'Sep 25, 2021',
+        to: 'Sep 25, 2021',
+        duration: '6:00 PM - 5:00 AM',
+    },
+    {
+        from: 'Sep 27, 2021',
+        to: 'Oct 1, 2021',
+        duration: '8:00 PM - 5:00 AM',
+    },
+    {
+        from: 'Oct 2, 2021',
+        to: 'Oct 2, 2021',
+        duration: '6:00 PM - 5:00 AM',
+    },
+    {
+        from: 'Oct 4, 2021',
+        to: 'Oct 8, 2021',
+        duration: '8:00 PM - 5:00 AM',
+    },
+    {
+        from: 'Oct 9, 2021',
+        to: 'Oct 9, 2021',
+        duration: '6:00 PM - 5:00 AM',
+    },
+    {
+        from: 'Oct 11, 2021',
+        to: 'Oct 15, 2021',
+        duration: '8:00 PM - 5:00 AM',
+    },
+    {
+        from: 'Oct 16, 2021',
+        to: 'Oct 16, 2021',
+        duration: '6:00 PM - 5:00 AM',
+    },
 ];
 
 module.exports = curfews;
