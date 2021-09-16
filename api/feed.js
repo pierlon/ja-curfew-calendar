ea68b4b0a967ab5b5c02e0bb9c242fbe5499625e
Pierre Gordon <pierregordon@protonmail.com>
2021-09-16 12:52:54 -0400

    Add feed api

---
+++ api/feed.js
@@ -0,0 +1,27 @@
+const curfews = require('../curfews');
+const generateEvent = require('../lib/generateEvent');
+const ical = require('ical-generator');
+
+const domain = 'ja-curfew-calendar.vercel.app';
+const path = '/feed';
+
+module.exports = (req, res) => {
+    if (req.method !== 'GET') {
+        return res.status(404);
+    }
+
+    const calendar = ical({
+        name: 'Jamaica Curfews',
+        prodId: `//$${domain}//Jamaica Curfew Calendar//EN`,
+        timezone: 'America/Jamaica',
+        ttl: 60 * 60, // 1 hour.
+        url: `https://${domain}/${path}`,
+    });
+
+    curfews.forEach(curfew => {
+        const event = generateEvent(curfew, calendar);
+        calendar.createEvent(event);
+    });
+
+    calendar.serve(res);
+  };
