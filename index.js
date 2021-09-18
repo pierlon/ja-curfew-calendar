fb2f76c1603cdb5f4b206458a0f5fe53077b03c2
Pierre Gordon <pierregordon@protonmail.com>
2021-09-18 14:34:15 -0400

    Refactor local setup

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const http = require('http');
 const ical = require('ical-generator');
 const generateEvent = require('./lib/generateEvent');
 
-const calendar = ical({
-    name: 'Jamaica Curfews',
-    prodId: '//ja-curfew-calendar.vercel.app//Jamaica Curfew Calendar//EN',
-    timezone: 'America/Jamaica',
-    ttl: 60 * 60 * 24, // 1 day
-    url: 'https://ja-curfew-calendar.vercel.app/feed.ical',
-});
-
-curfews.forEach(curfew => {
-    const event = generateEvent(curfew, calendar);
-    calendar.createEvent(event);
-}); 
-
 const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
     if (req.url === '/feed') {
+        const calendar = ical({
+            name: 'Jamaica Curfews',
+            prodId: '//ja-curfew-calendar.vercel.app//Jamaica Curfew Calendar//EN',
+            timezone: 'America/Jamaica',
+            ttl: 60 * 60 * 24, // 1 day
+            url: 'https://ja-curfew-calendar.vercel.app/api/feed',
+        });
+
+        curfews.forEach(curfew => {
+            const event = generateEvent(curfew, calendar);
+            calendar.createEvent(event);
+        });
+
         calendar.serve(res);
         return;
     }
